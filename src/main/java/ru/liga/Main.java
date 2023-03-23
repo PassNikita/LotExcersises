3235ece481590a18633b50eea3ac037c65736cd4
Nikita Trinitatskiy <NTrinitatskiy@gmail.com>
2023-03-23 11:20:36 +0300

    tes1

--- src/main/java/ru/liga/Main.java
+++ src/main/java/ru/liga/Main.java
@@ -1,7 +1,7 @@
 package ru.liga;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         System.out.println("Hello world!");
     }
 }
